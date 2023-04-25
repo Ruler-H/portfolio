df3721ebb71307b8ba22948a6d808c7942e2025c
hruler <elwl5515@gmail.com>
2023-04-25 23:44:25 +0900

    portfolio Initial commit

---
+++ src/test/java/hruler/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package hruler.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
